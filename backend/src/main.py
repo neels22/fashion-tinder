2e49c7177bc2c28529407486c4b5da27429dffd7
neels22 <indraneelsarode@gmail.com>
2025-12-17 00:09:20 -0800

    basic endpoint created

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-
+from image_generation import create_multiple_images, create_single_image
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/multiple_images")
+async def generate_multiple_images():
+    return create_multiple_images()
+
+@app.get("/single_image")
+async def generate_single_image():
+    return create_single_image()
