c54b85cbbe3e7017d605d6130b67c700a5bdc4d5
neels22 <indraneelsarode@gmail.com>
2025-12-18 12:06:36 -0800

    cors added

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI, Query, HTTPException, status, Body
+from fastapi.middleware.cors import CORSMiddleware
 from .scripts.image_generation import create_multiple_images, create_single_image
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
